85af9fb77053be264109a6b3845629e3a48a41af
Garikina Babji <garikina.babji@gmail.com>
2026-07-16 00:20:20 +0530

    Create appointment_raw table in SQL schema

--- sql/raw/02_appointment_raw.sql
+++ sql/raw/02_appointment_raw.sql
@@ -1,1 +1,18 @@
 
+
+
+CREATE TABLE IF NOT EXISTS `vivalab-healthcare.vivalab_dev_raw.appointment_raw`
+(
+    appointment_id STRING NOT NULL,
+    patient_id STRING,
+    doctor_id STRING,
+    department STRING,
+    appointment_date DATE,
+    appointment_time TIME,
+    status STRING,
+    reason STRING,
+
+    created_timestamp TIMESTAMP,
+    source_file_name STRING,
+    ingestion_timestamp TIMESTAMP
+);
