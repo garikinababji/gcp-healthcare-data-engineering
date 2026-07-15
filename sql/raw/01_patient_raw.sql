c10751c25b66b60c3580e520acd1037e127eb90a
Garikina Babji <garikina.babji@gmail.com>
2026-07-15 16:18:51 +0530

    Create 01_patient_raw.sql

---
+++ sql/raw/01_patient_raw.sql
@@ -0,0 +1,22 @@
+
+
+CREATE TABLE `vivalab-healthcare.vivalab_dev_raw.patient_raw`
+(
+    patient_id STRING NOT NULL,
+    first_name STRING,
+    last_name STRING,
+    gender STRING,
+    date_of_birth DATE,
+    blood_group STRING,
+    phone STRING,
+    email STRING,
+    city STRING,
+    state STRING,
+    diagnosis STRING,
+    doctor_id STRING,
+    admission_date DATE,
+
+    created_timestamp TIMESTAMP,
+    source_file_name STRING,
+    ingestion_timestamp TIMESTAMP
+);
