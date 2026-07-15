f5828a400b2694bfd4298c4a5dbb6969cb3315f2
Garikina Babji <garikina.babji@gmail.com>
2026-07-15 19:11:17 +0530

    Update 01_patient_raw.sql

--- sql/raw/01_patient_raw.sql
+++ sql/raw/01_patient_raw.sql
@@ -20,3 +20,26 @@ CREATE TABLE `vivalab-healthcare.vivalab_dev_raw.patient_raw`
     source_file_name STRING,
     ingestion_timestamp TIMESTAMP
 );
+
+# or we can use this query also
+    
+CREATE TABLE IF NOT EXISTS `vivalab-healthcare.vivalab_dev_raw.patient_raw`
+(
+    patient_id STRING NOT NULL,
+    first_name STRING,
+    last_name STRING,
+    gender STRING,
+    date_of_birth DATE,
+    blood_group STRING,
+    phone STRING,
+    email STRING,
+    city STRING,
+    state STRING,
+    diagnosis STRING,
+    doctor_id STRING,
+    admission_date DATE,
+
+    created_timestamp TIMESTAMP,
+    source_file_name STRING,
+    ingestion_timestamp TIMESTAMP
+);
