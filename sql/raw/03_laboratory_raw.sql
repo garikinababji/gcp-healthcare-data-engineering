e8c77692d51b0a1824e670415950178ae70841cb
Garikina Babji <garikina.babji@gmail.com>
2026-07-16 00:07:40 +0530

    Create laboratory_raw table in SQL schema

--- sql/raw/03_laboratory_raw.sql
+++ sql/raw/03_laboratory_raw.sql
@@ -1,1 +1,18 @@
 
+
+
+CREATE TABLE IF NOT EXISTS `vivalab-healthcare.vivalab_dev_raw.laboratory_raw`
+(
+    lab_id STRING NOT NULL,
+    patient_id STRING,
+    test_name STRING,
+    test_category STRING,
+    result STRING,
+    unit STRING,
+    normal_range STRING,
+    test_date DATE,
+
+    created_timestamp TIMESTAMP,
+    source_file_name STRING,
+    ingestion_timestamp TIMESTAMP
+);
