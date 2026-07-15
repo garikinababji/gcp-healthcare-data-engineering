3543d20da001d853d88eaaf9c592e4728b9dfe6d
Garikina Babji <garikina.babji@gmail.com>
2026-07-16 00:14:34 +0530

    Create doctor_raw table for healthcare data

--- sql/raw/05_doctor_raw.sql
+++ sql/raw/05_doctor_raw.sql
@@ -1,1 +1,17 @@
 
+
+
+CREATE TABLE IF NOT EXISTS `vivalab-healthcare.vivalab_dev_raw.doctor_raw`
+(
+    doctor_id STRING NOT NULL,
+    doctor_name STRING,
+    specialization STRING,
+    department STRING,
+    phone STRING,
+    email STRING,
+    experience_years INT64,
+
+    created_timestamp TIMESTAMP,
+    source_file_name STRING,
+    ingestion_timestamp TIMESTAMP
+);
