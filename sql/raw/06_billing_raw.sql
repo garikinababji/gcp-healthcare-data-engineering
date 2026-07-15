43b5ee656c9f2a7cae28064be730d15e7a2db9ba
Garikina Babji <garikina.babji@gmail.com>
2026-07-16 00:16:24 +0530

    Create billing_raw table for billing data

--- sql/raw/06_billing_raw.sql
+++ sql/raw/06_billing_raw.sql
@@ -1,1 +1,18 @@
 
+
+
+CREATE TABLE IF NOT EXISTS `vivalab-healthcare.vivalab_dev_raw.billing_raw`
+(
+    bill_id STRING NOT NULL,
+    patient_id STRING,
+    consultation_fee NUMERIC,
+    lab_fee NUMERIC,
+    medicine_fee NUMERIC,
+    total_amount NUMERIC,
+    payment_status STRING,
+    payment_date DATE,
+
+    created_timestamp TIMESTAMP,
+    source_file_name STRING,
+    ingestion_timestamp TIMESTAMP
+);
