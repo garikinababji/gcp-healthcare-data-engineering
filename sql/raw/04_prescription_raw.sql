88e319fe331184f2e5dbc9a0b1c5bae797a7f745
Garikina Babji <garikina.babji@gmail.com>
2026-07-16 00:09:03 +0530

    Create prescription_raw table in SQL schema

--- sql/raw/04_prescription_raw.sql
+++ sql/raw/04_prescription_raw.sql
@@ -1,1 +1,16 @@
 
+
+CREATE TABLE IF NOT EXISTS `vivalab-healthcare.vivalab_dev_raw.prescription_raw`
+(
+    prescription_id STRING NOT NULL,
+    patient_id STRING,
+    doctor_id STRING,
+    medicine_name STRING,
+    dosage STRING,
+    frequency STRING,
+    duration_days INT64,
+
+    created_timestamp TIMESTAMP,
+    source_file_name STRING,
+    ingestion_timestamp TIMESTAMP
+);
